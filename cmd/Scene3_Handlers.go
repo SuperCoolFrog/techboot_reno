f93da38a5659321c26fe142eac9055f4ec692133
Your Name <you@example.com>
2026-07-13 23:57:31 -0400

    Labels

--- cmd/Scene3_Handlers.go
+++ cmd/Scene3_Handlers.go
@@ -33,6 +33,8 @@ func Scene3_HandleInit(current, next GameState, gs *GridSystem, anims *Animation
 	gs.EnableGrid(gridId)
 
 	// Border
+	dividerX := 36
+
 	// .Corners
 	gs.SetCellSprite(gridId, 0, 0, assets.SpriteIDCornerTopLeft)
 	gs.SetCellSprite(gridId, 0, S3GridYCount-1, assets.SpriteIDCornerBottomLeft)
@@ -52,6 +54,26 @@ func Scene3_HandleInit(current, next GameState, gs *GridSystem, anims *Animation
 	for i := 1; i < S3GridXCount-1; i++ {
 		gs.SetCellSprite(gridId, i, 0, assets.SpriteIDHorizontalBar)
 	}
+	// ...Commands
+	hdrCmdX := dividerX/2 - 4
+	gs.Set(gridId, hdrCmdX+1, 0, CellTypeChar, 'C')
+	gs.Set(gridId, hdrCmdX+2, 0, CellTypeChar, 'O')
+	gs.Set(gridId, hdrCmdX+3, 0, CellTypeChar, 'M')
+	gs.Set(gridId, hdrCmdX+4, 0, CellTypeChar, 'M')
+	gs.Set(gridId, hdrCmdX+5, 0, CellTypeChar, 'A')
+	gs.Set(gridId, hdrCmdX+6, 0, CellTypeChar, 'N')
+	gs.Set(gridId, hdrCmdX+7, 0, CellTypeChar, 'D')
+	gs.Set(gridId, hdrCmdX+8, 0, CellTypeChar, 'S')
+
+	// ...Output
+	rightPanelHeaderX := S3GridXCount - dividerX/2
+	gs.Set(gridId, rightPanelHeaderX+1, 0, CellTypeChar, 'O')
+	gs.Set(gridId, rightPanelHeaderX+2, 0, CellTypeChar, 'U')
+	gs.Set(gridId, rightPanelHeaderX+3, 0, CellTypeChar, 'T')
+	gs.Set(gridId, rightPanelHeaderX+4, 0, CellTypeChar, 'P')
+	gs.Set(gridId, rightPanelHeaderX+5, 0, CellTypeChar, 'U')
+	gs.Set(gridId, rightPanelHeaderX+6, 0, CellTypeChar, 'T')
+
 	// ..Bottom
 	for i := 1; i < S3GridXCount-1; i++ {
 		gs.SetCellSprite(gridId, i, S3GridYCount-1, assets.SpriteIDHorizontalBar)
@@ -59,18 +81,23 @@ func Scene3_HandleInit(current, next GameState, gs *GridSystem, anims *Animation
 
 	// Dividers
 	// .Vertical
-	dividerX := 36
 	gs.SetCellSprite(gridId, dividerX, 0, assets.SpriteIDDownConnectBar)
 	for i := 1; i < S3GridYCount-1; i++ {
 		gs.SetCellSprite(gridId, dividerX, i, assets.SpriteIDVerticalBar)
 	}
 	gs.SetCellSprite(gridId, dividerX, S3GridYCount-1, assets.SpriteIDUpConnectBar)
 	// .Horizontal
-	gs.SetCellSprite(gridId, dividerX, S3GridYCount-11, assets.SpriteIDRightConnectBar)
+	verticalY := S3GridYCount - 11
+	gs.SetCellSprite(gridId, dividerX, verticalY, assets.SpriteIDRightConnectBar)
 	for i := 1; i < S3GridXCount-dividerX; i++ {
-		gs.SetCellSprite(gridId, dividerX+i, S3GridYCount-11, assets.SpriteIDHorizontalBar)
+		gs.SetCellSprite(gridId, dividerX+i, verticalY, assets.SpriteIDHorizontalBar)
 	}
-	gs.SetCellSprite(gridId, S3GridXCount-1, S3GridYCount-11, assets.SpriteIDLeftConnectBar)
+	gs.SetCellSprite(gridId, S3GridXCount-1, verticalY, assets.SpriteIDLeftConnectBar)
+	// ...Logs
+	gs.Set(gridId, rightPanelHeaderX+1, verticalY, CellTypeChar, 'L')
+	gs.Set(gridId, rightPanelHeaderX+2, verticalY, CellTypeChar, 'O')
+	gs.Set(gridId, rightPanelHeaderX+3, verticalY, CellTypeChar, 'G')
+	gs.Set(gridId, rightPanelHeaderX+4, verticalY, CellTypeChar, 'S')
 
 	S3History = make([][]byte, S3HistoryCapacity)
 	S3History[0] = make([]byte, S3BufferCapacity)

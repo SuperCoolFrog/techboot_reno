d119175fe3fa8324da20ee887868e6c6b1b3856e
Your Name <you@example.com>
2026-07-31 22:10:14 -0400

    Wip - building grid, working with counter

--- cmd/puzzles.pl
+++ cmd/puzzles.pl
@@ -12,7 +12,30 @@ cell_state(current_vertical).
 cell_state(current_horizontal).
 
 % mod X, // Y
-% setup(TotalCells, Rows, Cols, Idx, [Out|Rest]) :-
-    
-    
-    
+setup(TotalCells, Rows, Grid) :-
+    grid_rows(TotalCells, Rows, TotalCells, Grid).
+
+
+grid_rows(TotalCells, Rows, Remaining, Counter) :-
+    Remaining > 0,
+    Consume is TotalCells // Rows,
+    NuRemaining is Remaining - Consume,  
+    grid_rows(TotalCells, Rows, NuRemaining, RestCounter),
+    Counter is RestCounter + 1.
+
+
+grid_rows(_, _, 0, 0).
+
+
+% grid_rows(TotalCells, Rows, Remaining, Counter) :-
+%     Remaining > 0,
+%     Consume is TotalCells // Rows,
+%     NuRemaining is Remaining - Consume,  
+%     grid_rows(TotalCells, Rows, NuRemaining, RestCounter),
+%     Counter is RestCounter + 1.
+%
+%
+% grid_rows(_, _, 0, 0).
+
+
+
